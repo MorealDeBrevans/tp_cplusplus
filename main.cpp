6b8915d1b3b27d4d514f116368e69d64edc82787
Kyndt Martin <martin.kyndt@insa-lyon.fr>
2018-04-05 16:26:31 +0200

    debug

--- main.cpp
+++ main.cpp
@@ -7,6 +7,13 @@ int main() {
 	std::cout << s.c_str() << std::endl;
 	printf("%d \n",s.length()); 
 	printf("%d \n",s.max_size()); 
+	
+	s.resize(100);
+	printf("%d \n",s.length()); 
+	s.resize(100,'f');
+	std::cout << s.c_str() << std::endl;
+	s.resize(20); 
+	std::cout << s.c_str() << std::endl;
 	return 0;
 	
 }
