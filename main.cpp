3bec5220e4207644b8ba07a16cced2f9bd602539
Kyndt Martin <martin.kyndt@insa-lyon.fr>
2018-04-05 14:38:51 +0200

    code

--- main.cpp
+++ main.cpp
@@ -2,9 +2,11 @@
 #include <iostream>
 
 int main() {
-	String s("A character sequence");
+	String s("A character sequence which has a oihnregohneqogteqhzh inhatgr                     ii i iiiii");
 
 	std::cout << s.c_str() << std::endl;
+	printf("%d \n",s.length()); 
+	printf("%d \n",s.max_size()); 
 	return 0;
-
+	
 }
